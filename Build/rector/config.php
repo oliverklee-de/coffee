923bfabc49c42363e7858089d98b5e7df5696201
Oliver Klee <typo3-coding@oliverklee.de>
2026-08-17 19:18:45 +0200

    [TASK] Switch to Composer-based Rector sets (#598)

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -3,23 +3,16 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/../../src/',
         __DIR__ . '/../../tests/',
     ])
-    ->withPhpVersion(PhpVersion::PHP_83)
-    ->withPhpSets(true)
+    ->withPhpSets()
+    ->withComposerBased(phpunit: true)
     ->withSets([
-        // Rector sets
-
-        LevelSetList::UP_TO_PHP_83,
-
         // SetList::CODE_QUALITY,
         // SetList::CODING_STYLE,
         // SetList::DEAD_CODE,
@@ -29,13 +22,5 @@ return RectorConfig::configure()
         // SetList::PRIVATIZATION,
         // SetList::STRICT_BOOLEANS,
         SetList::TYPE_DECLARATION,
-
-        // PHPUnit sets
-
-        PHPUnitSetList::PHPUNIT_100,
-        PHPUnitSetList::PHPUNIT_110,
-        PHPUnitSetList::PHPUNIT_120,
-
-        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
     ])
     ->withImportNames(importShortClasses: false, removeUnusedImports: true);
